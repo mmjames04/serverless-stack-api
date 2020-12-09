1f8d43003471e9f8399dd5515448e302157d1397
James Corey <james.corey@alchemysystems.com>
2020-12-09 14:06:30 -0600

    Adding a timeout

--- services/notes/get.js
+++ services/notes/get.js
@@ -7,7 +7,7 @@ export const main = handler(async (event, context) => {
     // 'Key' defines the partition key and sort key of the item to be retrieved
     Key: {
       userId: event.requestContext.identity.cognitoIdentityId, // The id of the author
-      // noteId: event.pathParameters.id, // The id of the note from the path
+      noteId: event.pathParameters.id, // The id of the note from the path
     },
   };
 
@@ -15,6 +15,9 @@ export const main = handler(async (event, context) => {
   if (!result.Item) {
     throw new Error("Item not found.");
   }
+
+  // Set a timeout
+  await new Promise(resolve => setTimeout(resolve, 10000));
 
   // REturn the retrieved item
   return result.Item;
